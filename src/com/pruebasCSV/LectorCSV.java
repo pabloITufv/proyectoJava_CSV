19fce83759efac96d9a10c4256fe0ee80f6f357a
“PabloC” <7736022@alumnos.ufv.es>
2022-10-19 20:24:11 +0200

    LeerCSV creado

---
+++ src/com/pruebasCSV/LectorCSV.java
@@ -0,0 +1,15 @@
+package com.pruebasCSV;
+
+/*
+Esta clase se encarga de leer el archivo .csv que se le pase por parametro.
+No devuelve nada, sino que muestra el contenido del csv por terminal.
+ */
+public class LectorCSV {
+    /**
+     * @author PabloC
+     */
+
+    public void leeCSV(String nombreArchivo){
+
+    }
+}
